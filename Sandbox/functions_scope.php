20db5097db111aa6b5ef9496d3693c7e6645505a
Atheeq <atheeqmvj@gmail.com>
2024-11-09 14:38:23 +0400

    function_scope, defining,arguments,defaults, return values, debugging;

---
+++ Sandbox/functions_scope.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<br>
+    <head>
+        <title>
+FUNCTIONS: Scope 
+        </title>
+    </head>
+<body>
+<?php
+$bar = "outside"; //global scope
+
+function foo() {
+     
+    global $bar;
+    
+    if(isset($bar)){
+        echo "foo: " . "$bar" , "<br />";
+       
+    }
+    $bar = "inside"; // local scope
+}
+
+echo "1: " . $bar . "<br />";
+foo();
+echo "2: " . $bar . "<br />";
+ 
+?>
+</body>
+</html>
